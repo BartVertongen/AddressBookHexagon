0e85a0aba137bd4bc2cb8e2a85bb42bde6961b31
Bart Vertongen <bartvertongen70@gmail.com>
2021-09-05 15:08:57 +0200

    Added UseCase Tests as a Console application.

---
+++ PerfectSoftware/UseCasesTestConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+
+namespace UseCasesTestConsole
+{
+    class Program
+    {
+        static UseCase1 oUseCase1;
+        static UseCase4 oUseCase4;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here we test all the UseCases!");
+
+            oUseCase1 = new UseCase1();
+            oUseCase1.Execute();
+
+            //oUseCase4 = new UseCase4();
+            //oUseCase4.Execute();
+        }
+    }
+}
---
+++ PerfectSoftware/UseCasesTestConsole/UseCase1.cs
@@ -0,0 +1,123 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using PS.AddressBook.Business;
+
+
+namespace UseCasesTestConsole
+{
+    /// <summary>
+    /// Give Overview of All Contacts with possible filtering.
+    /// </summary>
+    public class UseCase1
+    {
+        private AddressBook _AddressBook;
+        private List<ContactLine> _ResultList;
+        private string _Filter;
+
+        public UseCase1()
+        {
+            this.PreCondition();
+        }
+
+        public void Execute()
+        {
+            this.Step1();
+            this.Step2();
+            this.Step3();
+        }
+
+        /// <summary>
+        /// You need an AddressBook with Contacts.
+        /// </summary>
+        public void PreCondition()
+        {
+            _AddressBook = new AddressBook();
+            _AddressBook.XmlFile = "AddressBookUseCase1.xml";
+            if (File.Exists(Environment.CurrentDirectory + "\\" + _AddressBook.XmlFile))
+            {
+                File.Delete(Environment.CurrentDirectory + "\\" + _AddressBook.XmlFile);
+            }
+            this.CreateAddressBookUseCase1();
+            _Filter = "";
+        }
+
+        /// <summary>
+        /// START:TRIGGER: USER asks for an overview of the adress Book Contacts with possible filtering.
+        /// </summary>
+        public void Step1()
+        {
+            Console.WriteLine($"UseCase1: Give Overview of All Contacts with possible filtering.");
+            Console.Write($"Give in the filter you want to Use: ");
+            _Filter = Console.ReadLine();
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// The SYSTEM gets all Contacts from the DB passing the filter.
+        /// </summary>
+        public void Step2()
+        {
+            this._ResultList = _AddressBook.GetOverview(_Filter);
+        }
+
+        /// <summary>
+        /// END: The SYSTEM shows the collected Contacts.
+        /// POSTCONDITION: We should have a list of the Contacts filtered by the given filter string.
+        /// </summary>
+        public void Step3()
+        {
+            if (this._ResultList.Count == 0)
+            {
+                Console.WriteLine("There are no Contact to show!");
+            }
+            else
+            {
+                string CurrentLetter, PreviousLetter = "";
+
+                foreach (ContactLine oContactLn in this._ResultList)
+                {
+                    CurrentLetter = oContactLn.Name.Substring(0, 1);
+                    if (PreviousLetter != CurrentLetter)
+                    {
+                        Console.WriteLine($"Tab [{CurrentLetter}]");
+                        PreviousLetter = CurrentLetter;
+                    }
+                    Console.WriteLine($"\t{oContactLn.Id}) {oContactLn.Name, -60} {oContactLn.ContentsCode}");
+                }
+            }
+        }
+
+        private void CreateAddressBookUseCase1()
+        {
+            Contact NewContact;
+            Address NewAddress;
+
+            NewContact = new Contact();
+            NewContact.Name = "An Dematras";
+            NewContact.PhoneNumber = "02/5820103";
+            _AddressBook.Add(NewContact);
+
+            NewContact = new Contact();
+            NewContact.Name = "André Hazes";
+            NewContact.Email = "andre@heaven.com";
+            _AddressBook.Add(NewContact);
+
+            NewContact = new Contact();
+            NewContact.Name = "Jan Franchipan";
+            NewContact.Email = "jan@eigenbelang.be";
+            _AddressBook.Add(NewContact);
+
+            NewContact = new Contact();
+            NewContact.Name = "Josephine DePin";
+            NewContact.Email = "jospin@proximus.be";
+            NewContact.PhoneNumber = "054/44.87.26";
+            NewAddress = new Address("Weverijstraat 12", "9500", "Geraardsbergen");
+            NewContact.Address = NewAddress;
+            _AddressBook.Add(NewContact);
+
+            _AddressBook.Save();
+        }
+    }
+}
