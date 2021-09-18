8f5310c7c556bcc7f42742af680979e5128a1a50
Bart Vertongen <bartvertongen70@gmail.com>
2021-09-18 23:26:45 +0200

    Added logging and configuration to the AddressBook Application.

--- PerfectSoftware/AddressBookConsoleApp/Program.cs
+++ PerfectSoftware/AddressBookConsoleApp/Program.cs
@@ -1,7 +1,10 @@
 ﻿//By Bart Vertongen copyright 2021.
 
 using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 using PS.AddressBook.Business.Interfaces;
 using PS.AddressBook.Data.Interfaces;
 using PS.AddressBook.UI;
@@ -13,9 +16,19 @@ namespace PS.AddressBook.ConsoleApp
 {
     class Program
     {
+        static IConfigurationRoot Configuration;
+
         private static void ConfigureServices(IServiceCollection services)
         {
             // Add application services.
+            // Add logging
+            services.AddSingleton(LoggerFactory.Create(builder =>
+            {
+                builder.AddSerilog(dispose: true); //Takes care of ILogger
+            }));
+            services.AddLogging();
+            // Add access to generic IConfigurationRoot
+            services.AddSingleton(Configuration);
             services.AddTransient<IAddressBook, BussAddressBook>();
             services.AddTransient<IConsoleUserInterface, ConsoleUserInterface>();
             services.AddTransient<IAddressBookUICommandFactory, AddressBookUICommandFactory>();
@@ -24,16 +37,45 @@ namespace PS.AddressBook.ConsoleApp
 
         private static void Main(string[] args)
         {
+            // Initialize serilog logger
+            Log.Logger = new LoggerConfiguration()
+                 .MinimumLevel.Debug()
+                 .WriteTo.File("AddressBookCLI.log")
+                 .CreateLogger();
+
+
+            // Build configuration
+            Log.Information("Creating configuration.");
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            Log.Information("Creating service collection.");
             IServiceCollection services = new ServiceCollection();
             ConfigureServices(services);
+
+            Log.Information("Building service provider");
             IServiceProvider serviceProvider = services.BuildServiceProvider();
 
-            var service = serviceProvider.GetService<IAddressBookCLIService>();
-            service.Run();
+            Log.Information("Starting service");
+            try
+            {
+                serviceProvider.GetService<IAddressBookCLIService>().Run();
+                Log.Information("Ending service");
 
-            Console.WriteLine("AddressBookService has completed.");
-            Console.WriteLine("Press ENTER to exit!");
-            Console.ReadLine();
+                Console.WriteLine("AddressBookService has completed.");
+                Console.WriteLine("Press ENTER to exit!");
+                Console.ReadLine();
+            }
+
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Error running service");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
--- PerfectSoftware/AdressBook.UI/UICommands/UpdateContactCommand.cs
+++ PerfectSoftware/AdressBook.UI/UICommands/UpdateContactCommand.cs
@@ -14,7 +14,7 @@ namespace PS.AddressBook.UI.Commands
         private readonly BussAddressBook _AddressBook;
         private readonly IConsoleUserInterface _UserInterface;
         private readonly IAddressBookUICommandFactory _CommandFactory;
-        private readonly Contact _Contact;
+        private Contact _UpdatedContact, _OriginalContact;
 
         /// <summary>
         /// The Command to create and add a Contact.
@@ -34,24 +34,67 @@ namespace PS.AddressBook.UI.Commands
 
         public string Description { get; } = "Update an existing Contact from the AddressBook.";
 
+        private void GetUpdatedContact()
+        {
+            _UpdatedContact = new Contact(_AddressBook);
+            _UpdatedContact.Name = _OriginalContact.Name;
+
+            //Street
+            _UserInterface.WriteMessage($"The current value for the street and number is {_OriginalContact.Address.Street}.");
+            _UpdatedContact.Address.Street = _UserInterface.ReadValue("Give in 'XX' to keep this value or type in another value: ");
+            if (_UpdatedContact.Address.Street.ToUpper() == "XX")
+                _UpdatedContact.Address.Street = _OriginalContact.Address.Street;
+
+            //Postal Code.
+            _UserInterface.WriteMessage($"The current value for the postal code is {_OriginalContact.Address.PostalCode}.");
+            _UpdatedContact.Address.PostalCode = _UserInterface.ReadValue("Give in 'XX' to keep this value or type in another value: ");
+            if (_UpdatedContact.Address.PostalCode.ToUpper() == "XX")
+                _UpdatedContact.Address.PostalCode = _OriginalContact.Address.PostalCode;
+
+            //Town
+            _UserInterface.WriteMessage($"The current value for the town is {_OriginalContact.Address.Town}.");
+            _UpdatedContact.Address.Town = _UserInterface.ReadValue("Give in 'XX' to keep this value or type in another value: ");
+            if (_UpdatedContact.Address.Town.ToUpper() == "XX")
+                _UpdatedContact.Address.Town = _OriginalContact.Address.Town;
+
+            //Phone
+            _UserInterface.WriteMessage($"The current value for the phone number is {_OriginalContact.PhoneNumber}.");
+            _UpdatedContact.PhoneNumber = _UserInterface.ReadValue("Give in 'XX' to keep this value or type in another value: ");
+            if (_UpdatedContact.PhoneNumber.ToUpper() == "XX")
+                _UpdatedContact.PhoneNumber = _OriginalContact.PhoneNumber;
+
+            //Email
+            _UserInterface.WriteMessage($"The current value for the email is {_OriginalContact.Email}.");
+            _UpdatedContact.Email = _UserInterface.ReadValue("Give in 'XX' to keep this value or type in another value: ");
+            if (_UpdatedContact.Email.ToUpper() == "XX")
+                _UpdatedContact.Email = _OriginalContact.Email;
+        }
 
         public (bool WasSuccessful, bool IsTerminating) Run(string argument="")
         {
 
             try
             {
-                if (_Contact.IsValid())
+                //Select an existing Contact
+                SelectContactCommand SelectCommand = new SelectContactCommand(_AddressBook, _UserInterface);
+                SelectCommand.Run();
+
+                //Get the original selected Contact
+                _OriginalContact = (Contact)_AddressBook.GetContact(SelectCommand.SelectedContactName);
+                //Get the new values
+                this.GetUpdatedContact();
+                if (_UpdatedContact.IsValid())
                 {
-                    _AddressBook.Update(_Contact);
+                    _AddressBook.Update(_UpdatedContact);
                     _AddressBook.Save();
-                    _UserInterface.WriteMessage($"The Contact with Name '{_Contact.Name}' is updated.");
+                    _UserInterface.WriteMessage($"The Contact with Name '{_UpdatedContact.Name}' is updated.");
                     return (true, false);
                 }
                 else
                 {
                     string Line;
 
-                    if (!_Contact.Address.IsValid())
+                    if (!_UpdatedContact.Address.IsValid())
                         Line = $"The Contact could not be updated because the Address was not valid!";
                     else
                         Line = $"The Contact could not be updated because the new values were not valid!";
@@ -63,7 +106,7 @@ namespace PS.AddressBook.UI.Commands
             {
                 string Line;
 
-                Line = $"An Error Occurred in UpdateContact Command with ContactName={_Contact.Name}.";
+                Line = $"An Error Occurred in UpdateContact Command with ContactName={_UpdatedContact.Name}.";
                 _UserInterface.WriteError(Line);
                 _UserInterface.WriteError("The error description is " + ex.Message);
                 return (false, false);
